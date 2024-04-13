f7de4d32ece05348fc6621a210714414d6c8dc72
Kshitij <75658936+Kshitijijari09@users.noreply.github.com>
2024-04-13 11:39:21 -0700

    Time: 468 ms (73.71%), Space: 42.6 MB (25.40%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,8 +1,7 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # creating a matrix of mxn and use dynamic programming
         
-        dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1) + 1)]
+        dp = [[0 for i in range(len(text2)+1)] for j in range(len(text1)+1)]
         
         for i in range(len(text1)-1,-1,-1):
             for j in range(len(text2)-1,-1,-1):
@@ -10,6 +9,7 @@ class Solution:
                     dp[i][j] = 1 + dp[i+1][j+1]
                 else:
                     dp[i][j] = max(dp[i][j+1],dp[i+1][j])
-                
+                    
         return dp[0][0]
-        
+            
+            
