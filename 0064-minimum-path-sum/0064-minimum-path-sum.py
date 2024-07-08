fb6cc805f1c19fdcfc234f07bdde08e655e41fd3
Kshitij <75658936+Kshitijijari09@users.noreply.github.com>
2024-07-07 19:15:58 -0700

    Time: 97 ms (17.57%), Space: 18.4 MB (24.63%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,13 +1,34 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid),len(grid[0])
+#         rows, cols = len(grid),len(grid[0])
         
-        res = [[float("inf")] * (cols + 1) for i in range(rows + 1)]
-        res[rows - 1][cols] = 0
+#         res = [[float("inf")] * (cols + 1) for i in range(rows + 1)]
+#         res[rows - 1][cols] = 0
         
-        for r in range(rows-1,-1,-1):
-            for c in range(cols-1,-1,-1):
-                res[r][c] = grid[r][c] + min(res[r+1][c],res[r][c+1])
-                print(res[r][c])
-        return res[0][0]
-                
+#         for r in range(rows-1,-1,-1):
+#             for c in range(cols-1,-1,-1):
+#                 res[r][c] = grid[r][c] + min(res[r+1][c],res[r][c+1])
+#                 print(res[r][c])
+#         return res[0][0]
+          
+    
+        n = len(grid)
+        m = len(grid[0])
+
+        # Edit the first row:
+        for i in range(1, m):
+            grid[0][i] = grid[0][i-1] + grid[0][i]
+        
+
+        # Edit the first col:
+        for i in range(1, n):
+            grid[i][0] = grid[i-1][0] + grid[i][0]
+        
+            
+        # Edit the remaining values in the grid accordingly!
+        for i in range(1, n):
+            for j in range(1, m):
+                print(grid[i-1][j],grid[i][j-1]+grid[i][j])
+                grid[i][j] = min(grid[i-1][j], grid[i][j-1]) + grid[i][j]
+
+        return grid[-1][-1]
