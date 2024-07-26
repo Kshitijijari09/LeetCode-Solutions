7ce04f0b9abc067e9c56f829e2a441f96ac6ca2d
Kshitij <75658936+Kshitijijari09@users.noreply.github.com>
2024-07-25 17:09:52 -0700

    Time: 31 ms (91.78%), Space: 16.7 MB (23.38%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -2,12 +2,12 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
-            
+           
         for i, letter_group in enumerate(zip(*strs)):
             print(i,letter_group)
             print(set(letter_group))
             if len(set(letter_group)) > 1:
                 return strs[0][:i]
+        
         else:
             return min(strs)
-            
