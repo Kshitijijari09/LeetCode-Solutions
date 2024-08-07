0396e1481c9afa5140ed45e7e1ab313d013354de
Kshitij <75658936+Kshitijijari09@users.noreply.github.com>
2024-08-07 11:10:36 -0700

    Time: 226 ms (99.81%), Space: 17.6 MB (43.88%) - LeetHub

---
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numberOfWays(self, s: str) -> int:
+        N = len(s)
+        ans = 0
+        
+        ones = 0
+        zeroes = 0
+        zero_one = 0
+        one_zero = 0
+        
+        for char in s:
+            
+            
+            if char == "1":
+                ans += one_zero
+                zero_one += zeroes
+                ones += 1
+                
+                
+            else:
+                ans += zero_one
+                one_zero += ones
+                zeroes += 1
+        
+        return ans
