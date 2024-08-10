7dec82ae33dfde341c4b082a4f09b3aad4e651f8
Kshitij <75658936+Kshitijijari09@users.noreply.github.com>
2024-08-09 20:11:40 -0700

    Time: 35 ms (71.55%), Space: 16.7 MB (27.72%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -2,30 +2,29 @@ class Solution:
     def reorganizeString(self, s: str) -> str:
         char_counts = Counter(s)
         max_count, letter = 0, ''
-        for char, count in char_counts.items():
-            if count > max_count:
-                max_count = count
+        
+        for char, counts in char_counts.items():
+            if counts > max_count:
+                max_count = counts
                 letter = char
-        if max_count > (len(s) + 1) // 2:
+        if max_count > (len(s)+1) // 2:
             return ""
-        ans = [''] * len(s)
+        ans = [""]*len(s)
         index = 0
-
-        # Place the most frequent letter
+        
         while char_counts[letter] != 0:
             ans[index] = letter
             index += 2
             char_counts[letter] -= 1
-
-        # Place rest of the letters in any order
-        for char, count in char_counts.items():
-            while count > 0:
+        
+        for char, counts in char_counts.items():
+            while counts > 0:
                 if index >= len(s):
                     index = 1
                 ans[index] = char
                 index += 2
-                count -= 1
-
+                counts -= 1
+        print(ans)
         return ''.join(ans)
                 
 
