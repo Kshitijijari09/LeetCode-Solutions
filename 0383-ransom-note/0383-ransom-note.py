d3f60e9824829e00d70cdd50d960f2972e737fc4
Kshitij <75658936+Kshitijijari09@users.noreply.github.com>
2024-10-14 11:10:02 -0700

    Time: 58 ms (53.52%), Space: 16.7 MB (31.76%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        rn = Counter(ransomNote)
+        mg = Counter(magazine)
+        
+        for key in rn:
+            if key not in mg:
+                return False
+            else:
+                if rn[key] > mg[key]:
+                    return False
+        return True
